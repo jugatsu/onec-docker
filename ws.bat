3f15fae300364f04cf6d150d728b92f0ae400cd7
Alexander Strizhachuk <a.strizhachuk@petrovich.ru>
2020-06-11 11:20:16 +0300

    не подходит под текущую концепцию

--- ws.bat
+++
@@ -1,6 +0,0 @@
-docker build ^
-  --build-arg ONEC_USERNAME=%ONEC_USERNAME% ^
-  --build-arg ONEC_PASSWORD=%ONEC_PASSWORD% ^
-  --build-arg ONEC_VERSION=%ONEC_VERSION% ^
-  -t %DOCKER_USERNAME%/ws:%ONEC_VERSION% ^
-  -f ws/Dockerfile .
